1d5e9fe7dc09a529baf57189b48b5b7bf724145d
Administrator <Administrator@BM-20150505AQSY>
2015-06-22 14:11:59 +0800

    hello

--- src/main/java/com/outwit/gamepalt/common/NameReuqestMapping.java
+++
@@ -1,7 +0,0 @@
-package com.outwit.gamepalt.common;
-
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-public class NameReuqestMapping extends RequestMappingHandlerMapping{
-
-}
